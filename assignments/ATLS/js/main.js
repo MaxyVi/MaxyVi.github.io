04b9970acb7b938bf9b786415df7bf24e5609744
Max Vali <91088382+MaxyVi@users.noreply.github.com>
2022-10-12 11:29:18 -0600

    Update main.js

--- assignments/ATLS/js/main.js
+++ assignments/ATLS/js/main.js
@@ -5,12 +5,12 @@ document.addEventListener("mouseover", changeIamge);
 document.addEventListener("mouseout", mouseout);
 
 function changeIamge() {
-    console.log("yay");
     div.style.color = "red";
-    img.src = "max-hood.png";
+    img.src = "../js/max-senior2.png"
   }
   
   function mouseout() {
     div.style.color = "black";
-    img.src = "max-senior2.png";
+    img.src = "../js/max-hood.png";
+    document.get
   }
